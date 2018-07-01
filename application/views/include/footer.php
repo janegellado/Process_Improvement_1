cc93ef7e0831b68e3b27e857f45ac6a9aaadf8d7
janegellado <janelynanngellado@gmail.com>
2018-07-01 08:54:05 +0800

    delete files

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -14,29 +14,6 @@
     <script src="<?php echo base_url('assets/css/vendors/jquery/dist/jquery.min.js'); ?>"></script>
     <!-- Bootstrap -->
     <script src="<?php echo base_url('assets/css/vendors/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
-    <!-- FastClick -->
-    <script src="<?php echo base_url('assets/css/vendors/fastclick/lib/fastclick.js'); ?>"></script>
-    <!-- NProgress -->
-    <script src="<?php echo base_url('assets/css/vendors/nprogress/nprogress.js'); ?>"></script>
-    <!-- Chart.js -->
-    <script src="<?php echo base_url('assets/css/vendors/Chart.js/dist/Chart.min.js'); ?>"></script>
-    <!-- jQuery Sparklines -->
-    <script src="<?php echo base_url('assets/css/vendors/jquery-sparkline/dist/jquery.sparkline.min.js'); ?>"></script>
-    <!-- Flot -->
-    <script src="<?php echo base_url('assets/css/vendors/Flot/jquery.flot.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/css/vendors/Flot/jquery.flot.pie.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/css/vendors/Flot/jquery.flot.time.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/css/vendors/Flot/jquery.flot.stack.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/css/vendors/Flot/jquery.flot.resize.js'); ?>"></script>
-    <!-- Flot plugins -->
-    <script src="<?php echo base_url('assets/css/vendors/flot.orderbars/js/jquery.flot.orderBars.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/css/vendors/flot-spline/js/jquery.flot.spline.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/css/vendors/flot.curvedlines/curvedLines.js'); ?>"></script>
-    <!-- DateJS -->
-    <script src="<?php echo base_url('assets/css/vendors/DateJS/build/date.js'); ?>"></script>
-    <!-- bootstrap-daterangepicker -->
-    <script src="<?php echo base_url('assets/css/vendors/moment/min/moment.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/css/vendors/bootstrap-daterangepicker/daterangepicker.js'); ?>"></script>
     
     <!-- Custom Theme Scripts -->
     <script src="<?php echo base_url('assets/css/build/js/custom.min.js'); ?>"></script>
--- application/views/include/header.php
+++ application/views/include/header.php
@@ -13,20 +13,14 @@
     <link href="<?php echo base_url('assets/css/vendors/bootstrap/dist/css/bootstrap.min.css'); ?>" rel="stylesheet">
     <!-- Font Awesome -->
     <link href="<?php echo base_url('assets/css/vendors/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="<?php echo base_url('assets/css/vendors/nprogress/nprogress.css'); ?>" rel="stylesheet">
-    <!-- iCheck -->
-    <link href="<?php echo base_url('assets/css/vendors/iCheck/skins/flat/green.css'); ?>" rel="stylesheet">
+    
     <!-- Datatables -->
     <link href="<?php echo base_url('assets/css/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css'); ?>" rel="stylesheet">
-     <!-- bootstrap-wysiwyg -->
-    <link href="../vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
-    <!-- Select2 -->
-    <link href="../vendors/select2/dist/css/select2.min.css" rel="stylesheet">
+ 
     <!-- Switchery -->
     <link href="../vendors/switchery/dist/switchery.min.css" rel="stylesheet">
     <!-- starrr -->
@@ -151,11 +145,6 @@
                   </ul>
                 </li>
 
-                </nav>
-                  </ul>
-                </li>
-              </ul>
-            
           </div>
         </div>
         <!-- /top navigation -->
