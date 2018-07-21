e3f82d193b90569f46906e6569d90cfc806c7be3
frnchscmarie <chesca.cortez@gmail.com>
2018-07-22 00:12:39 +0800

    Training views including admin (to be edited still)

---
+++ application/views/trainingadmin_view.php
@@ -0,0 +1,73 @@
+ <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+
+<div class="right_col" role="main">
+          <div class="">
+    
+          <div class="clearfix"></div>
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Training Attended</h2>
+                    <div class="clearfix"></div>
+                  </div>
+                    <div class="clearfix"></div>
+                    <div class="x_content" >
+                      <p class="text-muted font-13 m-b-30">Training Details.</p>
+                    <div>&nbsp;</div>
+
+                        <div class="x_content">
+                            <div class="col-md-12" style=" text-align: center; ">
+                                <table id="datatable" class="table table-striped table-bordered">
+                      <thead>
+  
+                          <tr id="trHead">
+                            <th rowspan="2">Employee ID</th>          
+                            <th rowspan="2">Employee Name</th>
+                            <th rowspan="2">Title of Seminar/Conference/Workshop/Short Courses<br>(write in full)</th>
+                            <th colspan="2">Inclusive Dates</th>
+                            <th rowspan="2">Number of Hours</th>
+                            <th rowspan="2">Conducted/Sponsored by<br>(Write in full)</th>
+                            <th rowspan="2">Attachments</th>
+                          </tr>
+
+                          <tr id="trHead">
+                            <th >From</th>          
+                            <th >To</th>
+                           
+                          </tr>
+
+                      </thead>
+
+
+                       <tbody>
+          
+                         <tr>
+                          <td>11/15/2018</td>
+                          <td>15-037-044</td>
+                          <td>Franchesca Marie Cortez</td>
+                          <td>Vacation Leave</td>
+                          <td>7</td>
+                          <td>00</td>
+                          <td>00</td>
+                          <td>00</td>
+                          
+                        </tr>
+                      </tbody>
+                     
+                     </table>
+
+            
+                              </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
