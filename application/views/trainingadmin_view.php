2bc48859a75143a90610989bb60c2fa1a1d1af2f
frnchscmarie <chesca.cortez@gmail.com>
2018-07-22 14:26:46 +0800

    Training and admin views done

--- application/views/trainingadmin_view.php
+++ application/views/trainingadmin_view.php
@@ -20,42 +20,40 @@
 
                         <div class="x_content">
                             <div class="col-md-12" style=" text-align: center; ">
-                                <table id="datatable" class="table table-striped table-bordered">
+                              <!--search bar and buttons-->
+                              <div class="pull-right" style="padding-bottom: 10px;">
+                                <input type="text" name="" placeholder="Search..">
+                                <a href="" class="btn btn-success btn-xs"><i class="fa fa-search"></i> Search</a>
+                                <a href="" class="btn btn-success btn-xs"><i class="fa fa-print"></i> Print</a>
+                              </div>
+
+                    <table id="datatable" class="table table-striped table-bordered">
                       <thead>
   
                           <tr id="trHead">
                             <th rowspan="2">Employee ID</th>          
                             <th rowspan="2">Employee Name</th>
                             <th rowspan="2">Title of Seminar/Conference/Workshop/Short Courses<br>(write in full)</th>
-                            <th colspan="2">Inclusive Dates</th>
+                            <th colspan="2" style="text-align: center;">Inclusive Dates<br><label style="margin-left: 0%;">From</label><label style="margin-left: 30%;">To</label></th>
                             <th rowspan="2">Number of Hours</th>
                             <th rowspan="2">Conducted/Sponsored by<br>(Write in full)</th>
                             <th rowspan="2">Attachments</th>
                           </tr>
-
-                          <tr id="trHead">
-                            <th >From</th>          
-                            <th >To</th>
-                           
-                          </tr>
-
                       </thead>
-
-
-                       <tbody>
+                        <tbody>
           
-                         <tr>
-                          <td>11/15/2018</td>
-                          <td>15-037-044</td>
-                          <td>Franchesca Marie Cortez</td>
-                          <td>Vacation Leave</td>
-                          <td>7</td>
-                          <td>00</td>
-                          <td>00</td>
-                          <td>00</td>
-                          
-                        </tr>
-                      </tbody>
+                           <tr>
+                            <td>11/15/2018</td>
+                            <td>15-037-044</td>
+                            <td>Franchesca Marie Cortez</td>
+                            <td>11/15/2018</td>
+                            <td>11/20/2018</td>
+                            <td>00</td>
+                            <td>00</td>
+                            <td>00</td>
+                            
+                          </tr>
+                         </tbody>
                      
                      </table>
 
