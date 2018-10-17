6955a2e92b6a877bbd81d3fea6094619d846e308
Xandra Bello <alexandrabello0130@gmail.com>
2018-10-17 23:22:59 +0800

    updated database

--- rbsd.sql
+++ rbsd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.1
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2018 at 01:37 PM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.2.6
+-- Generation Time: Oct 17, 2018 at 05:18 PM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,7 +30,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `employee` (
   `employeeID` varchar(20) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `mname` varchar(50) NOT NULL,
   `pg_level` varchar(10) NOT NULL,
   `birthday` varchar(15) NOT NULL,
   `date_hired` varchar(15) NOT NULL,
@@ -38,18 +40,32 @@ CREATE TABLE `employee` (
   `email` varchar(50) NOT NULL,
   `promo_date` varchar(20) NOT NULL,
   `civil_stat` varchar(20) NOT NULL,
-  `cp_no` int(20) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `cp_no` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`employeeID`, `name`, `pg_level`, `birthday`, `date_hired`, `position`, `email`, `promo_date`, `civil_stat`, `cp_no`, `username`, `password`) VALUES
-('12345', 'janelyn ann gellado', '6', '08/08/1999', '06/18/2018', 'ojt', 'janelynanngellado@gmail.com', 'N/A', 'single', 0, 'jane', '12345'),
-('12345', 'janelyn ann gellado', '6', '08/08/1999', '06/18/2018', 'ojt', 'janelynanngellado@gmail.com', 'N/A', 'single', 2147483647, 'jane', '12345');
+INSERT INTO `employee` (`employeeID`, `fname`, `lname`, `mname`, `pg_level`, `birthday`, `date_hired`, `position`, `email`, `promo_date`, `civil_stat`, `cp_no`) VALUES
+('01', 'Janelyn', 'Gellado', '', '6', '08/08/2016', '06/18/2018', 'OJT', 'janelynanngellado@gmail.com', '06/18/2018', 'single', 12355),
+('02', 'Herchell Mari', 'Forteo', '', '6', '1111-11-11', '2222-02-22', 'xxx', 'xxxxx', '3333-03-31', 'SINGLE', 12345789),
+('03', 'Al Francis', 'Orias', 'Protacio', '5', '1998-11-07', '2018-09-17', 'OJT', 'alorias', '2018-09-17', 'single', 911111111),
+('04', 'Franchesca Marie', 'Cortez', 'Cadondon', '5', '11/15/98', '06/18/2018', 'OJT', 'chesca@gmail.com', '06/18/2018', 'single', 12355),
+('05', 'Alexandra', 'Bello', '', '5', '01/30/99', '06/18/2018', 'OJT', 'xandra@gmail.com', '06/18/2018', 'single', 12355),
+('06', 'Red', 'Aricayos', 'Mauhay', '10', '2018-10-18', '2018-10-11', 'Admin', 'red@gmail.com', '2018-10-12', 'Single', 2147483647),
+('07', 'Noel', 'Benus', 'Danog', '10', '2018-10-06', '2018-10-19', 'Employee', 'Noel@gmail.com', '2018-10-12', 'Single', 2147483647);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_type`
+--
+
+CREATE TABLE `employee_type` (
+  `employetype_id` varchar(25) NOT NULL,
+  `employee_type` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -58,18 +74,44 @@ INSERT INTO `employee` (`employeeID`, `name`, `pg_level`, `birthday`, `date_hire
 --
 
 CREATE TABLE `leavedb` (
-  `employeeID` varchar(20) NOT NULL,
   `date_of_filing` varchar(20) NOT NULL,
   `place` text NOT NULL,
   `type` text NOT NULL,
-  `emarks` varchar(50) NOT NULL,
-  `ic_from` varchar(20) NOT NULL,
-  `ic_to` varchar(20) NOT NULL,
+  `inc_dates` date NOT NULL,
   `recommendation` varchar(50) NOT NULL,
   `supervisor` text NOT NULL,
   `no_of_days` varchar(20) NOT NULL,
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `leavedb`
+--
+
+INSERT INTO `leavedb` (`date_of_filing`, `place`, `type`, `inc_dates`, `recommendation`, `supervisor`, `no_of_days`, `status`) VALUES
+('09/17/2018', 'manila', 'Sick Leave', '0000-00-00', '', 'aaaa', '1', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` varchar(20) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `type` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`, `type`) VALUES
+('01', 'Janelyn', 'admin', 'admin'),
+('06', 'Red', '12345', 'superadmin'),
+('07', 'Noel', '12345', 'employee');
 
 -- --------------------------------------------------------
 
@@ -79,16 +121,18 @@ CREATE TABLE `leavedb` (
 
 CREATE TABLE `mr` (
   `employeeID` varchar(50) NOT NULL,
+  `employee_name` varchar(50) NOT NULL,
   `date_assigned` date NOT NULL,
   `qty` varchar(50) NOT NULL,
   `unit` varchar(50) NOT NULL,
-  `unit_name` varchar(50) NOT NULL,
+  `property_name` varchar(50) NOT NULL,
   `description` varchar(50) NOT NULL,
   `date_purchased` date NOT NULL,
   `property_no` varchar(50) NOT NULL,
   `classification_no` varchar(50) NOT NULL,
   `unit_value` varchar(50) NOT NULL,
-  `total_value` varchar(50) NOT NULL
+  `total_value` varchar(50) NOT NULL,
+  `mr_no` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -142,10 +186,28 @@ CREATE TABLE `training_sched` (
 --
 
 --
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`employeeID`);
+
+--
+-- Indexes for table `employee_type`
+--
+ALTER TABLE `employee_type`
+  ADD PRIMARY KEY (`employetype_id`);
+
+--
 -- Indexes for table `leavedb`
 --
 ALTER TABLE `leavedb`
   ADD PRIMARY KEY (`date_of_filing`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `mr`
